051df9c89a01433fe23b8304d442b4cbfb9e144b
auxeno <alex1goddard@gmail.com>
2026-03-31 21:19:12 +0100

    Lazily import adapters

--- parallax/adapters/__init__.py
+++ parallax/adapters/__init__.py
@@ -1,4 +1,19 @@
-from .brax import BraxAdapter
-from .gymnax import GymnaxAdapter
+import typing
+
+if typing.TYPE_CHECKING:
+    from .brax import BraxAdapter as BraxAdapter
+    from .gymnax import GymnaxAdapter as GymnaxAdapter
 
 __all__ = ["BraxAdapter", "GymnaxAdapter"]
+
+
+def __getattr__(name: str):
+    if name == "BraxAdapter":
+        from .brax import BraxAdapter
+
+        return BraxAdapter
+    if name == "GymnaxAdapter":
+        from .gymnax import GymnaxAdapter
+
+        return GymnaxAdapter
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
